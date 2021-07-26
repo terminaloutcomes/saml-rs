0827bdb7fef1dad1d6393727fd0dd45d4672690e
yaleman <yaleman@users.noreply.github.com>
2021-07-26 14:37:41 +0000

    deploy: 3c23d7bf4ece3a9c05df3073bf636e64de68d4eb

---
+++ saml_rs/sp/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["SamlBinding",""],["SamlBindingType","Allows one to build a definition with [SamlBindingType::AssertionConsumerService](s) and [SamlBindingType::SingleLogoutService](s)"]],"fn":[["xml_indent",""]],"struct":[["ServiceBinding",""],["SpMetadata",""]]});
