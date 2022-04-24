937b86263c14285b873a58902e03e8c1ca6a10c0
James Hodgkinson <james@terminaloutcomes.com>
2022-04-24 10:56:15 +1000

    fixing testing

--- test_examples.py
+++ test_examples.py
@@ -4,12 +4,16 @@
 # import lxml.etree as ET
 # https://signxml.readthedocs.io/en/latest/
 
-# from base64 import b64decode
+# because we're just reading the files
+#pylint: disable=consider-using-with
 
+from pathlib import Path
+
+import pytest
 from lxml import etree
 from signxml import XMLSigner, XMLVerifier, methods # type: ignore
 
-def main() -> None:
+def test_main() -> None:
     """ main func """
     # filename = '2021-08-26-saml-response-signed.txt'
     filename = '2021-08-26-saml-response-unsigned.txt'
@@ -18,9 +22,12 @@ def main() -> None:
     # with open(f"c14n_{filename}", mode='w', encoding='utf-8') as out_file:
     #     print(xml.etree.ElementTree.canonicalize(from_file=filename, out=out_file))
 
-    data_to_sign = open(filename, encoding="utf-8").read()
-    cert = open("/Users/yaleman/Downloads/kanidm_sp_test/m1-server.pem", encoding="utf-8").read()
-    key = open("/Users/yaleman/Downloads/kanidm_sp_test/m1-server-key.pem", encoding="utf-8").read()
+    file_to_sign = Path(filename, encoding="utf-8").expanduser().resolve()
+    if not file_to_sign.exists():
+        pytest.skip()
+    data_to_sign = file_to_sign.read_bytes()
+    cert = Path("~/Downloads/kanidm_sp_test/m1-server.pem").expanduser().resolve().read_bytes()
+    key = Path("/Users/yaleman/Downloads/kanidm_sp_test/m1-server-key.pem").expanduser().resolve().read_bytes()
     root = etree.fromstring(data_to_sign)
     signed_root = XMLSigner(method=methods.enveloped).sign(root, key=key, cert=cert)
     verified_data = XMLVerifier().verify(signed_root, x509_cert=cert).signed_xml
@@ -36,4 +43,4 @@ def main() -> None:
     # assertion_data = XMLVerifier().verify(b64decode("""), x509_cert=cert).signed_xml
 
 if __name__ == "__main__":
-    main()
+    test_main()
