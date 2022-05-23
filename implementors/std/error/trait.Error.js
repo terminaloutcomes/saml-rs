48862c8c59ee750a0855fda5a1c442acdac0bc61
yaleman <yaleman@users.noreply.github.com>
2022-05-23 11:33:19 +0000

    deploy: 30de4a80c2711937ad25a3c48bd8fdb066cd9713

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["saml_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"saml_rs/cert/struct.CertParseError.html\" title=\"struct saml_rs::cert::CertParseError\">CertParseError</a>","synthetic":false,"types":["saml_rs::cert::CertParseError"]}];
+implementors["saml_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"saml_rs/cert/struct.CertParseError.html\" title=\"struct saml_rs::cert::CertParseError\">CertParseError</a>","synthetic":false,"types":["saml_rs::cert::CertParseError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
